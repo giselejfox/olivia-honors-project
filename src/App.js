696e926b00f50109ada33692bc40e63ef5d1762e
Gisele Fox <72312641+giselejfox@users.noreply.github.com>
2024-01-05 11:02:43 -0800

    test commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
 
       {/* CHANGE THE BACKGROUND by changing the name of the file in the quotes here ---------***    (right now it's styrofoam-texture.jpeg)      */}
       <div className="background-texture vw-100 vh-100" style={{background: `url("img/styrofoam-texture.jpeg")`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}></div>
+    
+    
     </div>
   );
 }
